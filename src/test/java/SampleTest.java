ef7a7927a6d86dd77ab13e07dcee94e2f06cadf9
farukakyol <farukakyol441@gmail.com>
2021-08-30 15:09:28 +0300

    first commit

---
+++ src/test/java/SampleTest.java
@@ -0,0 +1,27 @@
+import org.junit.Test;
+
+public class SampleTest {
+
+
+    @Test
+    public void simpleTest(){
+
+
+        System.out.println("Faruk Akyol");
+    }
+
+
+    @Test
+    public void simpleTest2(){
+
+
+        System.out.println("Elif Akyol");
+    }
+
+    @Test
+    public void simpleTest3(){
+
+
+        System.out.println("Ahmet Akyol");
+    }
+}
